cac003b3f06b026a56716863868d7bf7f7ebb5c4
Alessandro Burato <ale.burato@icloud.com>
2024-10-25 15:48:10 +0200

    Split vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   base: "/external-oontacts-poc/",
 });
